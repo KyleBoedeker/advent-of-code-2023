8b1eb4ff733d2d798d57445a15a24782d2e76e0f
Kyle Boedeker <kyle.boedeker01@gmail.com>
2023-12-12 22:55:11 -0600

    Solve p08p2 via spoiler assist

--- p08p2.py
+++ p08p2.py
@@ -1,6 +1,14 @@
 import itertools
+import math
 from pathlib import Path
 import re
+
+"""
+I had to cheat via the subreddit and read the term "LCM" after getting bored
+of waiting for my brute-force approach to work. Just that term alone made
+me realize this was some clever math-major stuff based on cleverly designed
+input data. Sigh... not the fun kind of puzzle I was hoping for.
+"""
 
 
 def main():
@@ -14,17 +22,20 @@ def main():
         if match := re.match(R'(...) = \((...), (...)\)', line):
             node_map[match.group(1)] = (match.group(2), match.group(3))
 
-    steps = 0
-    current_nodes = tuple(k for k in node_map if k.endswith('A'))
-    cycler = itertools.cycle(instructions)
-    while True:
-        if all(n.endswith('Z') for n in current_nodes):
-            break
-        steps += 1
-        tuple_idx = 0 if next(cycler) == 'L' else 1
-        current_nodes = tuple(node_map[n][tuple_idx] for n in current_nodes)
+    path_lengths = []
+    for current_node in (n for n in node_map if n.endswith('A')):
+        steps = 0
+        cycler = itertools.cycle(instructions)
+        while True:
+            if current_node.endswith('Z'):
+                break
+            steps += 1
+            choices = node_map[current_node]
+            tuple_idx = 0 if next(cycler) == 'L' else 1
+            current_node = choices[tuple_idx]
+        path_lengths.append(steps)
 
-    print('Steps taken to go from AAA -> ZZZ:', steps)
+    print('Steps taken to go from all ??A -> to all ??Z:', math.lcm(*path_lengths))
 
 
 if __name__ == '__main__':
